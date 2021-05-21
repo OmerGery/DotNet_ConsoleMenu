4a86b852a2a1e9a22f7cc198c734a204aa69d1c9
Omer Gery <omergery1996@gmail.com>
2021-05-21 17:21:54 +0300

    AddedSomestuff

---
+++ Ex04.Menus.Delegates/ActionOption.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Ex04.Menus.Delegates
+{
+    public class ActionOption : Option
+    {
+        private Action m_Function;
+
+        public ActionOption(string i_Text, ref Action i_Function) : base(i_Text)
+        {
+            m_Function = i_Function;
+        }
+        
+       protected override void OnSelection()
+       {
+           m_Function();
+       }
+    }
+}
---
+++ Ex04.Menus.Delegates/NavigationOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Ex04.Menus.Delegates
+{
+    public class NavigationOption : Option
+    {
+        private MenuItem m_LinkedMenu;
+        public NavigationOption(string i_Text, ref MenuItem io_MenuToNavigateTo) : base(i_Text)
+        {
+            m_LinkedMenu = io_MenuToNavigateTo;
+        }
+
+        protected override void OnSelection()
+        { 
+            Console.Clear();
+            m_LinkedMenu.ShowOptions();
+        }
+    }
+}
