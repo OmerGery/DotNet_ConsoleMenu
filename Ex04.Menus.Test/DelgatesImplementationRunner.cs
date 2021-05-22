1e7c4c0d86445aa5340e0155f2607ffa6018cfa8
Omer Gery <omergery1996@gmail.com>
2021-05-22 11:28:29 +0300

    added all the options

---
+++ Ex04.Menus.Test/DelgatesImplementationRunner.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Ex04.Menus.Delegates;
+
+namespace Ex04.Menus.Test
+{
+    public class DelgatesImplementationRunner
+    {
+        private MainMenu m_MainMenu;
+        public MainMenu BuildMenu()
+        {
+            m_MainMenu = new MainMenu("Main Menu");
+            Menu mainScreen = m_MainMenu.MainScreen;
+            Menu versionAndSpacesMenu = new Menu("Version and Spaces Menu", ref mainScreen);
+            mainScreen.AddOption(new NavigationMenuItem("Version and Spaces", ref versionAndSpacesMenu));
+            Menu dateAndTime = new Menu("Show Date / Time", ref mainScreen);
+            mainScreen.AddOption(new NavigationMenuItem("Show Date / Time", ref dateAndTime));
+            versionAndSpacesMenu.AddOption(new ActionMenuItem("Show Version", TesterMethods.ShowVersion));
+            versionAndSpacesMenu.AddOption(new ActionMenuItem("Count Spaces", TesterMethods.CountSpaces));
+            dateAndTime.AddOption(new ActionMenuItem("Show Time", TesterMethods.ShowTime));
+            dateAndTime.AddOption(new ActionMenuItem("Show Date", TesterMethods.ShowDate));
+            return m_MainMenu;
+        }
+        public void RunMenuWithDelegatesImplementation()
+        {
+            BuildMenu();
+            m_MainMenu.Show();
+        }
+    }
+}
